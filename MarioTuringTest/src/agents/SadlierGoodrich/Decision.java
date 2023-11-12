8735c4c2aa121eb00b696465e4b246df6e386c67
cp-goodrich <goodrichcameron2@gmail.com>
2023-11-11 20:04:16 -0500

    Added Action, Decision, and FloatDecision java files (Still need to be implemented

---
+++ MarioTuringTest/src/agents/SadlierGoodrich/Decision.java
@@ -0,0 +1,35 @@
+package agents.SadlierGoodrich;
+
+import engine.core.MarioForwardModel;
+import engine.core.MarioTimer;
+import engine.helper.GameStatus;
+import engine.helper.MarioActions;
+
+public class Decision extends DecisionTree {
+	private boolean[] action;
+	//blah
+	
+
+
+	public boolean[] jump(MarioForwardModel model, MarioTimer timer) {
+		action[MarioActions.RIGHT.getValue()] = true;
+		action[MarioActions.SPEED.getValue()] = true;
+		action[MarioActions.JUMP.getValue()] = model.mayMarioJump() || !model.isMarioOnGround();
+	
+		return action;
+	}
+	
+	public boolean[] run(MarioForwardModel model, MarioTimer timer) {
+		action[MarioActions.RIGHT.getValue()] = true;
+		action[MarioActions.SPEED.getValue()] = true;
+		
+		return action;
+	}
+	
+	public boolean[] walk(MarioForwardModel model, MarioTimer timer) {
+		action[MarioActions.RIGHT.getValue()] = true;
+		action[MarioActions.SPEED.getValue()] = false;
+		
+		return action;
+	}
+}
